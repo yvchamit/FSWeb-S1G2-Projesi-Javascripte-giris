0bcc34d94401ca7288985c50ffcbc1a68ea93275
Hamit Yuvaci <yvchamit@gmail.com>
2026-01-04 03:34:27 -0600

    Görev 1d tamalandı

--- index.js
+++ index.js
@@ -52,8 +52,8 @@ Aşağıdakileri yaparak carpma isimli fonksiyonu tamamla:
    3. console.log(carpma(7,4)) ile yazdığın fonsiyonu test edin. Console'da sonucu 28 olarak görmelisin.
 */
 
-function carpma(/*buraya kodunu yazabilirsin*/) {
-  /*buraya kodunu yazabilirsin*/
+function carpma(sayi1, sayi2) {
+  return sayi1 * sayi2;
 }
 
 /* Görev 2 : Köpeğin Yaşı */
